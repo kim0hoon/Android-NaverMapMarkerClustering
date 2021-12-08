97e7ec98cf5f218e84d95a276815365b99a91fae
kim0hoon <gns9460@gmail.com>
2021-12-09 00:25:52 +0900

    feat: 화면에 지도 표시

--- app/src/main/java/com/example/navermapmarkerclustring/MainActivity.kt
+++ app/src/main/java/com/example/navermapmarkerclustring/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.navermapmarkerclustring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.NaverMapSdk
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NaverMapSdk.getInstance(this).client=
+            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_MAP_CLIENT_ID)
     }
 }
