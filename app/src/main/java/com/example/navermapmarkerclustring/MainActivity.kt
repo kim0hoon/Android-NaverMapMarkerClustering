c1a808d4ce58588a54c57704d4f8456b9f854801
kim0hoon <gns9460@gmail.com>
2021-12-10 22:05:59 +0900

    feat: ClusteringManager 생성

--- app/src/main/java/com/example/navermapmarkerclustring/MainActivity.kt
+++ app/src/main/java/com/example/navermapmarkerclustring/MainActivity.kt
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.navermapmarkerclustring.base.BaseActivity
 import com.example.navermapmarkerclustring.base.makeKoreaMap
+import com.example.navermapmarkerclustring.clustering.ClusteringManager
 import com.example.navermapmarkerclustring.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate),
     OnMapReadyCallback {
 
-    private lateinit var map:NaverMap
+    private lateinit var clusteringManager: ClusteringManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,7 +34,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-        map=naverMap
-        makeKoreaMap(map)
+        makeKoreaMap(naverMap)
+        clusteringManager = ClusteringManager(naverMap)
     }
 }
---
+++ app/src/main/java/com/example/navermapmarkerclustring/clustering/ClusteringManager.kt
@@ -0,0 +1,15 @@
+package com.example.navermapmarkerclustring.clustering
+
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.overlay.Marker
+
+class ClusteringManager(naverMap: NaverMap) {
+
+    fun clearMarker(){
+
+    }
+
+    fun addMarker(marker: Marker){
+
+    }
+}
