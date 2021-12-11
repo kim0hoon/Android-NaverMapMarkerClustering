fc9809fac0cc5b1340c4fd7acdd93b80e06a3064
kim0hoon <gns9460@gmail.com>
2021-12-11 22:07:24 +0900

    feat: ClusterData 생성

---
+++ app/src/main/java/com/example/navermapmarkerclustring/clustering/ClusterData.kt
@@ -0,0 +1,7 @@
+package com.example.navermapmarkerclustring.clustering
+
+import com.naver.maps.geometry.LatLng
+//TODO 마커에 사용할 데이터 추가
+open class ClusterData(val pos: LatLng,val title:String) {
+
+}
