9f479dbf6f3b33c53cdac225924737644f836665
kim0hoon <gns9460@gmail.com>
2021-12-11 22:37:33 +0900

    refactor: 상수 값 분리

--- app/src/main/java/com/example/navermapmarkerclustring/base/utility.kt
+++ app/src/main/java/com/example/navermapmarkerclustring/base/utility.kt
@@ -4,7 +4,13 @@ import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.NaverMap
 
-fun makeKoreaMap(naverMap: NaverMap){
-    naverMap.minZoom=5.8
-    naverMap.extent= LatLngBounds(LatLng(33.0,124.0),LatLng(43.0,132.0))
+const val KOREA_ZOOM_LEVEL = 5.8
+const val KOREA_MIN_LAT = 33.0
+const val KOREA_MAX_LAT = 43.0
+const val KOREA_MIN_LNG = 124.0
+const val KOREA_MAX_LNG = 132.0
+fun makeKoreaMap(naverMap: NaverMap) {
+    naverMap.minZoom = KOREA_ZOOM_LEVEL
+    naverMap.extent =
+        LatLngBounds(LatLng(KOREA_MIN_LAT, KOREA_MIN_LNG), LatLng(KOREA_MAX_LAT, KOREA_MAX_LNG))
 }
