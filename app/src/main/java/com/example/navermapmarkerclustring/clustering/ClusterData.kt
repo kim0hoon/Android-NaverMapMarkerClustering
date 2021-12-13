cba15c9e70c37fef8a444f638d5f7e1ae482cbe0
kim0hoon <gns9460@gmail.com>
2021-12-13 16:12:17 +0900

    feat: clusterBase 변수 추가

--- app/src/main/java/com/example/navermapmarkerclustring/clustering/ClusterData.kt
+++ app/src/main/java/com/example/navermapmarkerclustring/clustering/ClusterData.kt
@@ -3,5 +3,5 @@ package com.example.navermapmarkerclustring.clustering
 import com.naver.maps.geometry.LatLng
 //TODO 마커에 사용할 데이터 추가
 open class ClusterData(val pos: LatLng,val title:String) {
-
+    var clusterBase:ClusterData?=null
 }
